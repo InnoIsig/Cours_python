b3ae1960434a56582fb77d32d8d9e6567fe23af8
Innoisig <bahatiinnocent36@gmail.com>
2024-06-08 09:39:47 -0700

    Ajout de code

--- Dictionnaire.py
+++ Dictionnaire.py
@@ -3,10 +3,11 @@ Création du dictionnaire: dico = {} #vide
                           deco = {<cle>:<valeur>, <cle>:<valeur>}
 
 Accès dans un dictionnaire :  print(deco[cle])
-ajout dans le dictionnaire : 
+ajout et modification dans le dictionnaire : dico[<cle>] = "<valeur>"
+suppression :
 """
 
-dico = {"nom":"Innocent" }
+dico = {"nom":"Innocent", "ville":"Goma" }
 print(dico["nom"])
 
 #ajout de valeur dan un dictionnaire
@@ -15,3 +16,22 @@ print(dico)
 
 dico["autre nom"] = "Monsieur"
 print(dico)
+
+# Pour supprimer un element dans le dictionnaire
+dico.pop("autre nom")
+print(dico)
+
+
+# valeur_supprime = dico.pop("ville")
+# print(valeur_supprime)
+# dico.pop("ville")
+# print(dico)
+
+# Pour recuperer une valeur supprimer
+valeur_supprime = dico.pop("ville")
+print(valeur_supprime)
+
+if  "Innocent" in dico:
+    print("oui je sui la")
+else:
+    print("Non suis pas en l'interieur")
